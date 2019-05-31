b8bc85068e5211e99354cadd740c4131da413ad3
Hiren donda <dondahiren2@gmail.com>
2019-05-31 23:43:04 +0530

    Updates

---
+++ precache-manifest.100015ca61dd2adc73dfd88b30e78388.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "434870fef63be1681450d0a7e0782d1e",
+    "url": "/reactdemo/index.html"
+  },
+  {
+    "revision": "12738a933b9c60040b15",
+    "url": "/reactdemo/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/reactdemo/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "12738a933b9c60040b15",
+    "url": "/reactdemo/static/js/main.bc6d883c.chunk.js"
+  },
+  {
+    "revision": "d63cc1b5485118404932",
+    "url": "/reactdemo/static/js/runtime~main.dbcbabdd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactdemo/static/media/logo.5d5d9eef.svg"
+  }
+]);
